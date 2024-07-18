a5d21fc1c9ca280596d0a247d3da8a40a6b2da55
Zakharov Denis <zdv.work@yandex.ru>
2024-07-19 00:12:36 +0600

    add NotFaound page add PrivateRoute conponent add Routes

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,11 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainScreen from './pages/main-screen';
+import { AppRoute, AuthorizationStatus } from '../const';
+import LoginScreen from './pages/login-screen';
+import FavoritesScreen from './pages/favorites-screen';
+import OfferScreen from './pages/offer-sreen';
+import NotFoundScreen from './pages/not-found-sreen';
+import PrivateRoute from './private-route/private-route';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +13,36 @@ type AppProps = {
 
 function App({placesCount}: AppProps): JSX.Element {
   return (
-    <MainScreen placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path = {AppRoute.Main}
+          element = {<MainScreen placesCount={placesCount} />}
+        />
+        <Route
+          path = {AppRoute.Login}
+          element = {<LoginScreen />}
+        />
+        <Route
+          path = {AppRoute.Favorites}
+          element = {
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path = {AppRoute.Offer}
+          element = {<OfferScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
